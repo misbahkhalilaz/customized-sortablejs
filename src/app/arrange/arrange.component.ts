3513252329c637124adb02e6e522ce04acb722dd
Misbah Khalil <misbah.khalil.az@gmail.com>
2023-08-12 19:27:23 +0500

    applied sortable js

--- src/app/arrange/arrange.component.ts
+++ src/app/arrange/arrange.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
 import { AbstractArrangeDirective } from './abstract-arrange.directive';
+import Sortable from './sortable/Sortable';
 
 @Component({
   selector: 'app-arrange',
@@ -13,5 +13,19 @@ export class ArrangeComponent
   extends AbstractArrangeDirective
   implements OnInit
 {
-  ngOnInit() {}
+  ngOnInit() {
+    var nestedSortables = [].slice.call(
+      document.querySelectorAll('.nested-sortable')
+    );
+
+    // Loop through each nested sortable element
+    for (var i = 0; i < nestedSortables.length; i++) {
+      new Sortable(nestedSortables[i], {
+        group: 'nested',
+        animation: 150,
+        fallbackOnBody: true,
+        swapThreshold: 0.65,
+      });
+    }
+  }
 }
